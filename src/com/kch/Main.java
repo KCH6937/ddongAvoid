c6a05489758debf58148b53ef1be810c575a0ab9
JungHuNi <calendar8342@naver.com>
2019-11-01 19:16:33 +0900

    복구

--- src/com/kch/Main.java
+++ src/com/kch/Main.java
@@ -1,20 +1,4 @@
 package com.kch;
-// Main class
-
-<<<<<<< HEAD
-import javax.swing.*;
-
-public class Main {
-    public static final int SCREEN_WIDTH = 1200;    //게임전체 넓이 1200 고정
-    public static final int SCREEN_HEIGHT = 720;    //게임전체 높이 720 고정
-    public static int COUNT = 0; //게임 점수
-    final static int FPS = 15;
-
-    public static void main(String[] args) {
-        long score = System.currentTimeMillis();
-        long scoreProsess;
-        ScoreControl sc = new ScoreControl();
-=======
 import com.kch.entity.Shit;
 import com.kch.storage.FileStorage;
 
@@ -23,16 +7,30 @@ import java.io.File;
 public class Main {
     public static final int SCREEN_WIDTH = 600;    //게임전체 넓이 600 고정
     public static final int SCREEN_HEIGHT = 850;    //게임전체 높이 850 고정
+    public static int COUNT = 0; //게임 점수
+    final static int FPS = 15;
 
     public static void main(String[] args) {
         FileStorage.load();
->>>>>>> 6194663c8f9f3fee74082e7ac22d053833db273d
+
         Gui gui = new Gui();
+
+        long score = System.currentTimeMillis();
+        long scoreProsess;
+
+        long game = System.currentTimeMillis();
+        long gameProsess;
+        ScoreControl sc = new ScoreControl();
 
         //Shit s = new Shit(SCREEN_WIDTH, SCREEN_HEIGHT, 0.0f);
 
         while(true){
             scoreProsess = System.currentTimeMillis();
+            gameProsess = System.currentTimeMillis();
+
+            if(gameProsess - game >= (1000.0 / FPS)){
+
+            }
 
             if(scoreProsess - score >= (3000.0)) {
                 score = scoreProsess;
