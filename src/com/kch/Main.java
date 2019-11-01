5d4a913dc0db79ea00598882a494947b4c5308f7
JungHuNi <calendar8342@naver.com>
2019-11-01 17:53:37 +0900

    FPS 입힘

--- src/com/kch/Main.java
+++ src/com/kch/Main.java
@@ -1,13 +1,30 @@
 package com.kch;
 // Main class
 
+import javax.swing.*;
+
 public class Main {
     public static final int SCREEN_WIDTH = 1200;    //게임전체 넓이 1200 고정
     public static final int SCREEN_HEIGHT = 720;    //게임전체 높이 720 고정
+    public static int COUNT = 0; //게임 점수
+    final static int FPS = 15;
 
     public static void main(String[] args) {
+        long score = System.currentTimeMillis();
+        long scoreProsess;
+        ScoreControl sc = new ScoreControl();
         Gui gui = new Gui();
 
 
+        while(true){
+            scoreProsess = System.currentTimeMillis();
+
+            if(scoreProsess - score >= (3000.0)) {
+                score = scoreProsess;
+                ++COUNT;
+                sc.setScore(COUNT);
+                sc.scoreRender();
+            }
+        }
     }
 }
